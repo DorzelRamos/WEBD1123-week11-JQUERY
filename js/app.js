ca64eb0876dc5d07398025fcbfd6272dc5545631
DorzelRamos <dorzel.ramos@gmail.com>
2022-11-02 13:11:34 -0500

    added forms, buttons, and more

---
+++ js/app.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+//    $("div").hide()
+    // $("p").hide()
+    // let windowWidth = window.innerWidth;
+    // let windowHeight = window.innerHeight;
+    // console.log("This is the width " + windowWidth);
+    // console.log("This is the height " + windowHeight);
+    // if(windowWidth < 720) {
+    //     $("p").hide()
+    // }
+    // $("div > p").hide();
+
+    $("button#myBtn").click(function(){
+        $("p#myParagraph").hide();
+
+    })
+});
+
+//$ means jQuery
